90f97ce61c02919d4206cfeae34b08ac78109f5f
Cranberries <mr.smallwatermolen@gmail.com>
2021-07-08 23:25:47 +0800

    刷

---
+++ Leecode/930和相同的二元子数组.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        map<int,int> m;
+        int sum = 0, res = 0;
+        for(int& num:nums)
+        {
+             ++m[sum];
+            sum += num;
+            res += m[sum-goal];
+        }
+        return res;
+    }
+};
